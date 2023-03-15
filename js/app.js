59e269686023ea07b5366f45170a1e16b1980f3a
Jagtiani <kunaal.jagtiani@zuhlke.com>
2023-03-15 13:52:15 +0800

    Added functionality to review previous steps. Bug fixes and refactoring

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ class TicTacToe {
         this.playerIcon = "X";
         this.gameOver = false;
         this.cells = document.getElementsByTagName("td");
+        this.history = [];
+        this.currentIndex = 0;
     }
 
     togglePlayer() {
@@ -20,6 +22,7 @@ class TicTacToe {
 
     playTurn(cellId) {
         if(this.board[cellId] === "" && !this.gameOver) {
+            this.history.push(this.board.slice());
             this.board[cellId] = this.playerIcon;
             let cell = document.getElementById(cellId);
             cell.innerHTML = this.playerIcon;
@@ -28,6 +31,9 @@ class TicTacToe {
                 this.winningCombination.forEach(element => {
                     document.getElementById(element).classList.add("won");
                 });
+                this.displayReviewButtonsInUI();
+                this.history.push(this.board.slice());
+                this.currentIndex = this.history.length-1;
             } else {
                 this.togglePlayer();
                 this.updateCurrentPlayerInUI();
@@ -37,19 +43,21 @@ class TicTacToe {
 
     checkIfGameHasEnded() {
         this.gameOver = false;
-        for (let combination of this.winningCombinations) {
-            this.gameOver = combination.every(element => this.board[element] === this.playerIcon);
-            if (this.gameOver) {
-                this.winningCombination = combination;
-                break;
-            }
-        }
+        this.winningCombination = this.winningCombinations.find( combination => 
+            combination.every(element => this.board[element] === this.playerIcon)
+        );
+        this.gameOver = !!this.winningCombination;
         return this.gameOver;
     }
 
     resetBoard() {
         this.board = ["", "", "", "", "", "", "", "", ""];
-        this.currentPlayer = "X";
+        this.winningCombination = "";
+        this.currentPlayer = "1";
+        this.playerIcon = "X";
+        this.gameOver = false;
+        this.history = [];
+        this.currentIndex = history.length;
         for(let cell of cells) {
             cell.innerHTML="";
             cell.classList.remove("selected");
@@ -57,8 +65,44 @@ class TicTacToe {
         }
         this.updateCurrentPlayerInUI();
     }
-    
+
+    previousState() {
+        if(this.currentIndex > 0 && this.currentIndex <= this.history.length) {
+            this.currentIndex = this.currentIndex - 1;
+            this.board = this.history[this.currentIndex];
+            this.updateGrid();
+        }
+    }
+
+    nextState() {
+        if(this.currentIndex >= 0 && this.currentIndex < this.history.length - 1) {
+            this.currentIndex = this.currentIndex + 1;
+            this.board = this.history[this.currentIndex];
+            this.updateGrid();
+        }
+    }
+
+    updateGrid() {
+        for(let i = 0; i<this.cells.length; i++) {
+            this.cells[i].innerHTML = this.board[i];
+        }
+    }
+
     updateCurrentPlayerInUI() {
         document.getElementById("current-player").innerHTML="Current Player: " + this.currentPlayer;
+    }
+
+    displayReviewButtonsInUI() {
+        document.getElementById("next-button").hidden = false;
+        document.getElementById("previous-button").hidden = false;
+    }
+
+    updateUIIfCellIsEmpty() {
+        for(let cell of cells) {
+            if(cell.innerHTML==="") {
+                cell.classList.remove("selected");
+                cell.classList.remove("won");
+            }
+        }
     }
 }
