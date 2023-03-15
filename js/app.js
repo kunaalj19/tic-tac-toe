720f8a78a3ec51ca2d99411e18df982055093e07
Jagtiani <kunaal.jagtiani@zuhlke.com>
2023-03-15 10:34:04 +0800

    Use of a class to track the game state

--- js/app.js
+++ js/app.js
@@ -1,64 +1,64 @@
-let board = ["", "", "", "", "", "", "", "", ""];
-let cells = document.getElementsByTagName("td");
-
-const winningCombinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
-    [0, 4, 8], [2, 4, 6] // diagonal
-];
-
-let winningCombination = "";
-let currentPlayer = "1";
-let playerIcon = "X";
-let gameOver = false;
-
-function togglePlayer() {
-    currentPlayer = currentPlayer === "1" ? "2" : "1";
-    playerIcon = currentPlayer === "1" ? "X" : "O"
-}
-
-function playTurn(cellId) {
-    if(board[cellId] === "" && !gameOver) {
-        console.log(gameOver);
-        board[cellId] = playerIcon;
-        let cell = document.getElementById(cellId);
-        cell.innerHTML = playerIcon;
-        cell.classList.add("selected");
-        if(checkIfGameHasEnded()) {
-            winningCombination.forEach(element => {
-                document.getElementById(element).classList.add("won");
-            });
-            //alert("Player " + currentPlayer + " has won");
-        } else {
-            togglePlayer();
-            updateCurrentPlayer();
-        }
-    } 
-}
-
-function checkIfGameHasEnded() {
-    gameOver = false;
-    for (let combination of winningCombinations) {
-        gameOver = combination.every(element => board[element] === playerIcon);
-        if (gameOver) {
-            winningCombination = combination;
-            break;
-        }
+class TicTacToe {
+    constructor() {
+        this.board = ["", "", "", "", "", "", "", "", ""];
+        this.winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+            [0, 4, 8], [2, 4, 6] // diagonal
+        ];
+        this.winningCombination = "";
+        this.currentPlayer = "1";
+        this.playerIcon = "X";
+        this.gameOver = false;
+        this.cells = document.getElementsByTagName("td");
     }
-    return gameOver;
-}
 
-function resetBoard() {
-    board = ["", "", "", "", "", "", "", "", ""];
-    currentPlayer = "X";
-    for(let cell of cells) {
-        cell.innerHTML="";
-        cell.classList.remove("selected");
-        cell.classList.remove("won");
+    togglePlayer() {
+        this.currentPlayer = this.currentPlayer === "1" ? "2" : "1";
+        this.playerIcon = this.currentPlayer === "1" ? "X" : "O";
     }
-    updateCurrentPlayer();
-}
 
-function updateCurrentPlayer() {
-    document.getElementById("current-player").innerHTML="Current Player: " + currentPlayer;
+    playTurn(cellId) {
+        if(this.board[cellId] === "" && !this.gameOver) {
+            this.board[cellId] = this.playerIcon;
+            let cell = document.getElementById(cellId);
+            cell.innerHTML = this.playerIcon;
+            cell.classList.add("selected");
+            if(this.checkIfGameHasEnded()) {
+                this.winningCombination.forEach(element => {
+                    document.getElementById(element).classList.add("won");
+                });
+            } else {
+                this.togglePlayer();
+                this.updateCurrentPlayerInUI();
+            }
+        } 
+    }
+
+    checkIfGameHasEnded() {
+        this.gameOver = false;
+        for (let combination of this.winningCombinations) {
+            this.gameOver = combination.every(element => this.board[element] === this.playerIcon);
+            if (this.gameOver) {
+                this.winningCombination = combination;
+                break;
+            }
+        }
+        return this.gameOver;
+    }
+
+    resetBoard() {
+        this.board = ["", "", "", "", "", "", "", "", ""];
+        this.currentPlayer = "X";
+        for(let cell of cells) {
+            cell.innerHTML="";
+            cell.classList.remove("selected");
+            cell.classList.remove("won");
+        }
+        this.updateCurrentPlayerInUI();
+    }
+    
+    updateCurrentPlayerInUI() {
+        document.getElementById("current-player").innerHTML="Current Player: " + this.currentPlayer;
+    }
 }
